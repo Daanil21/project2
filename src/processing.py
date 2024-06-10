4fa84d1c2e5d571e6a80c0e48fe5ab58ec0384d0
Daniil Pankratov <daniilpankratov9@gmail.com>
2024-06-10 19:22:53 +0300

    new file

--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,7 @@
 data = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-           {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-           {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-           {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
 
 
 def filter_by_statefilter_by_state(my_list, state="EXECUTED"):
@@ -9,7 +9,7 @@ def filter_by_statefilter_by_state(my_list, state="EXECUTED"):
     значением по умолчанию EXECUTED) и возвращает новый список, содержащий только те словари, у которых ключ state
     содержит переданное в функцию значение."""
 
-    for list_one in my_list:
+    for _ in my_list:
         if state == "EXECUTED":
             return my_list[0:2]
         elif state == "CANCELED":
